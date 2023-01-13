eb5fe7910c4f7b378ce6df2be7f7a408a0c669b4
Truong Tri Tin <eriegin@nexondv.com>
2023-01-13 10:39:46 +0700

    adding me query

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,6 +1,14 @@
 import { Context } from "~/interfaces";
 
 export default {
+  me: (_: any, __: any, { prisma, userInfo }: Context) => {
+    if (!userInfo) return null;
+    return prisma.user.findUnique({
+      where: {
+        id: userInfo.userId,
+      },
+    });
+  },
   posts: async (_: any, __: any, { prisma }: Context) => {
     return prisma.post.findMany({
       orderBy: [{ createdAt: "desc" }],
--- src/schema.ts
+++ src/schema.ts
@@ -1,5 +1,6 @@
 export const typeDefs = `#graphql
   type Query {
+    me: User
     posts: [Post!]!
   }
 
